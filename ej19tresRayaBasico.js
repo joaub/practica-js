343d4aae6789bb3218946fb92b308e5f39f26f0a
Joaquin Ugarte <ugartejoa12@gmail.com>
2025-09-05 12:58:38 -0300

    empezando tateti basico

---
+++ ej19tresRayaBasico.js
@@ -0,0 +1,18 @@
+function tresEnRaya(tablero){
+    let tabla = ["","","",
+                "","","",
+                "","",""];
+    
+    const combinacionesGanadoras = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columnas
+        [0, 4, 8], [2, 4, 6]             // Diagonales
+    ];
+    let ganador = null;
+    for(let i=0; i<tablero.length; i++){
+        tabla[i] = tablero[i];
+    }
+
+    for (const combinacion of combinacionesGanadoras) {
+    } 
+}  
