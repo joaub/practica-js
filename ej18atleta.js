d5d437c3e1e11cd79b17780c5ca6805a3f83cd8d
Joaquin Ugarte <ugartejoa12@gmail.com>
2025-09-05 12:34:32 -0300

    haciendolo mas legible y arreglando error

--- ej18atleta.js
+++ ej18atleta.js
@@ -1,30 +1,32 @@
-function carrera(arr, pista){
+function carrera(arr, pista) {
     let pistaFinal = pista.split("");
     let tramoSuperado = true;
-    
-    for(let i = 0; i < arr.length; i++){
-        let correr = arr[i];
+
+    if (arr.length !== pistaFinal.length) {
+        throw new Error("La cantidad de acciones y la longitud de la pista deben ser iguales.");
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let accion = arr[i];
         let tramo = pistaFinal[i];
 
-        if(correr === "run" && tramo === "_" || correr === "jump" && tramo === "|"){
-            continue
-        
-        }else if(correr === "jump" && tramo === "_" ){
-            
+        if ((accion === "run" && tramo === "_") || 
+        (accion === "jump" && tramo === "|")) {
+            continue;
+        } else if (accion === "jump" && tramo === "_") {
             pistaFinal[i] = "x";
             tramoSuperado = false;
-        
-        }else if(correr === "run" && tramo === "|"){
-            
+        } else if (accion === "run" && tramo === "|") {
             pistaFinal[i] = "/";
             tramoSuperado = false;
-            
         }
-        
     }
-    console.log("resultado de la pista "+ pistaFinal.join(""));
+    console.log("resultado de la pista " + pistaFinal.join(""));
     return tramoSuperado;
 }
 console.log(carrera(["run", "run", "jump", "run"], "__|_")); // true
 console.log(carrera(["run", "run", "run", "run"], "_|_|")); // false
 console.log(carrera(["run", "jump", "jump", "run"], "_||_"));
+console.log(carrera(["run", "jump", "jump", "run"], "___|"));
+
+//subir denuevo este repo a github
