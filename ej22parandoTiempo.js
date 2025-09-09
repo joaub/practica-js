851a1c173131f7f275691c9b1cbc4fbc3ab0abc2
Joaquin Ugarte <ugartejoa12@gmail.com>
2025-09-09 16:00:29 -0300

    parando el tiempo funcion async

--- ej22parandoTiempo.js
+++ ej22parandoTiempo.js
@@ -1,7 +1,17 @@
-function parandoTiempo(nr1, nr2) {
-    let suma = nr1 + nr2;
-    setTimeout(() => {
-        console.log(suma);
-    }, 2000);  
+function parandoTiempo(nr1, nr2, segundos) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(nr1 + nr2);
+        }, segundos *1000);  
+    });
 }
-console.log(parandoTiempo(3, 4));
+
+async function ejecutar(nr1, nr2, segundos) {
+    console.log('Empieza la ejecución');
+    let resultado = await parandoTiempo(nr1, nr2, segundos);
+    console.log("resultado ",resultado);
+
+    console.log('Termina la ejecución');
+}
+ejecutar(2, 4, 3);
+ejecutar(5, 8, 6);
